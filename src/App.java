84947f52b95d4760774fc9df15c83a0b6b189133
Zsomi4 <you@example.com>
2022-10-03 11:18:28 +0200

    Kezdés

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void bubiRendez(int[] tomb) {
+        
+    }
+    public static void main(String[] args) throws Exception {
+        int[] tomb = {30, 25, 12, 40, 27, 38};
+        bubiRendez(tomb);
+    }
+}
