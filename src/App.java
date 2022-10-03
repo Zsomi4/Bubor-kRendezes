9c1bcc5388018d2bf22aa08fed1002f584be535b
Zsomi4 <you@example.com>
2022-10-03 11:23:37 +0200

    A 2 ciklus megírása a bubiRendez metódusban.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void bubiRendez(int[] tomb) {
+        for (int i = tomb.length-1; i < 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(tomb[j] > tomb[j+1]) {
+                    int swap = tomb[j];
+                    tomb[j] = tomb[j+1];
+                    tomb[j+1] = swap;
+                }
+            }
+        }
         
     }
     public static void main(String[] args) throws Exception {
